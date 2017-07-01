4721a149666b020894fb0499860bcb1a09e670a3
RonNofar <ronnofar2@gmail.com>
2017-07-01 14:11:59 -0700

    v0.0.2
    All of the v0.0.2 update that I forgot to push.

--- Bullet/Assets/Scripts/Enemy/EnemyGun.cs
+++ Bullet/Assets/Scripts/Enemy/EnemyGun.cs
@@ -2,34 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyGun : MonoBehaviour {
-
-    [SerializeField]
-    private GameObject EnemyBulletPrefab;
-
-	// Use this for initialization
-	void Start () {
-        Invoke("FireEnemyBullet", 1f);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-    void FireEnemyBullet()
+namespace Bullet.Enemy
+{
+    public class EnemyGun : MonoBehaviour
     {
-        GameObject playerShip = GameObject.Find("Player");
 
-        if (playerShip != null)
+        [SerializeField]
+        private GameObject EnemyBulletPrefab;
+
+        public float delay = 1f;
+
+        public int repeats = 0;
+        public float repeatDelay = 1f;
+
+        // Use this for initialization
+        void Start()
         {
-            GameObject bullet = (GameObject)Instantiate(EnemyBulletPrefab);
-            bullet.transform.position = transform.position;
-            Vector2 direction = playerShip.transform.position - bullet.transform.position;
-
-            bullet.GetComponent<EnemyBullet>().SetDirection(direction);
+            StartCoroutine(Util.Func.WaitAndRunAction(delay, () => { FireEnemyBullet(repeats, repeatDelay); }));
+            //Invoke("FireEnemyBullet", 1f);
         }
-        else
-            Debug.Log("playerShip is null");
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void FireEnemyBullet(int repeats = 0, float repeatDelay = 0, int i = 0)
+        {
+            GameObject playerShip = GameObject.Find("Player");
+
+            if (playerShip != null)
+            {
+                GameObject bullet = (GameObject)Instantiate(EnemyBulletPrefab);
+                bullet.transform.position = transform.position;
+                Vector2 direction = playerShip.transform.position - bullet.transform.position;
+
+                bullet.GetComponent<EnemyBullet>().SetDirection(direction);
+            }
+            else
+                Debug.Log("playerShip is null");
+            
+            if (i < repeats)
+            {
+                StartCoroutine(Util.Func.WaitAndRunAction(repeatDelay, () => { FireEnemyBullet(repeats, repeatDelay, ++i); }));
+            }
+        }
     }
 }
