07d5b1bb1872c051ba65cecf1ae296484665af6b
RonNofar <ronnofar2@gmail.com>
2017-07-14 16:01:18 -0700

    PlayerMaster added
    PlayerMaster script to replace PlayerItems.cs
    Changed playerMoneyNumber to Money
    Added PlayerMaster to _master object in _preload scene

---
+++ Bullet/Assets/PlayerMaster.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Bullet
+{
+    public class PlayerMaster : MonoBehaviour
+    {
+        public Item[] itemsUnlocked = new Item[5];
+        public float Money;
+
+        static public PlayerMaster Instance { get { return _instance; } }
+        static protected PlayerMaster _instance;
+
+        public bool isMouseMovement = false;
+
+        public void Awake()
+        {
+            if (_instance != null)
+            {
+                Debug.LogWarning("PlayerMaster is already in play. Deleting new!", gameObject);
+                Destroy(gameObject);
+            }
+            else
+            { _instance = this; }
+        }
+    }
+
+}
