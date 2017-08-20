2eac3dff543c58a242f2255ff74910848e72f5dc
RonNofar <ronnofar2@gmail.com>
2017-08-19 21:51:20 -0700

    Add Help Canvas

---
+++ Bullet/Assets/HelpCanvas.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Bullet
+{
+    public class HelpCanvas : MonoBehaviour
+    {
+
+        [SerializeField]
+        private GameObject[] objArr;
+        [SerializeField]
+        private float timeTillDeath = 5f;
+
+        [SerializeField]
+        private GameObject readyText;
+        [SerializeField]
+        private GameObject goText;
+
+        private void Start()
+        {
+            readyText.SetActive(false);
+            goText.SetActive(false);
+            for (int i = 0; i < objArr.Length; ++i)
+            {
+                objArr[i].SetActive(true);
+            }
+            StartCoroutine(Util.Func.WaitAndRunAction(timeTillDeath, 
+                () => {
+                    for (int i = 0; i < objArr.Length; ++i)
+                        objArr[i].SetActive(false);
+                    readyText.SetActive(true);
+                    StartCoroutine(Util.Func.WaitAndRunAction(0.5f, 
+                        () => {
+                            readyText.SetActive(false);
+                            goText.SetActive(true);
+                            StartCoroutine(Util.Func.WaitAndRunAction(0.5f, 
+                                () => { goText.SetActive(false); }));
+                        }));
+                }));
+        }
+
+
+    }
+}
